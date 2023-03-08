74fefe8e20ec79651142f31d4c34fb1f81ab0922
sor973 <zerostriketeam@gmail.com>
2023-03-08 17:49:30 -0500

    finish model and serializer

--- Main/models.py
+++ Main/models.py
@@ -11,13 +11,50 @@ class Department(models.Model):
 class User_privileges(models.Model):
     name = models.CharField(max_length=100)
 
+class Id_types(models.Model):
+    name = models.CharField(max_length=100)
+
+class Item_categories(models.Model):
+    name = models.CharField(max_length=100)
+
+class Item_statuses(models.Model):
+    name = models.CharField(max_length=100)
+
+class Borrow_statuses(models.Model):
+    name = models.CharField(max_length=100)
+
 class User(models.Model):
     u_name = models.CharField(max_length=200)
     u_password = models.CharField(max_length=200)
     u_email = models.CharField(max_length=200)
     u_tel = models.IntegerField()
-    u_faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='faculty_users')
-    u_department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='department_users')
-    u_privilege = models.ForeignKey(User_privileges, on_delete=models.CASCADE, related_name='privilege_users')
+    u_faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='users_faculty')
+    u_department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='users_department')
+    u_privilege = models.ForeignKey(User_privileges, on_delete=models.CASCADE, related_name='users_privilege')
     u_create_at = models.DateTimeField(auto_now_add=True)
     u_update_at = models.DateTimeField(auto_now=True)
+
+class Item (models.Model):
+    item_id = models.CharField(max_length=200)
+    item_id_type = models.ForeignKey(Id_types, on_delete=models.CASCADE, related_name='item_id_type')
+    item_name = models.CharField(max_length=200)
+    item_category = models.ForeignKey(Item_categories, on_delete=models.CASCADE, related_name='item_item_categories')
+    item_description = models.CharField(max_length=200)
+    item_faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='item_faculty')
+    item_department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='item_department')
+    item_status = models.ForeignKey(Item_statuses, on_delete=models.CASCADE, related_name='item_status')
+    item_borrow_status = models.ForeignKey(Borrow_statuses, on_delete=models.CASCADE, related_name='item_borrow_status')
+    item_note = models.CharField(max_length=200)
+    item_img_url = models.CharField(max_length=200)
+    item_create_at = models.DateTimeField(auto_now_add=True)
+    item_update_at = models.DateTimeField(auto_now=True)
+
+class Borrow_info (models.Model):
+    b_item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='borrow_info_item')
+    b_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='borrow_info_user')
+    b_location = models.CharField(max_length=200)
+    b_note =  models.CharField(max_length=200)
+    b_borrow_time = models.DateTimeField(auto_now=True)
+    b_return_time = models.DateTimeField(auto_now=True)
+    b_create_at = models.DateTimeField(auto_now_add=True)
+    b_update_at = models.DateTimeField(auto_now=True)
--- Main/serializers.py
+++ Main/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from Main.models import Faculty, Department, User_privileges, User
+from Main.models import Faculty, Department, User_privileges, Id_types, Item_categories, Item_statuses, Borrow_statuses, User, Item, Borrow_info
 
 class FacultySerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,6 +16,26 @@ class User_privilegesSerializer(serializers.ModelSerializer):
         model = User_privileges
         fields = ['id', 'name']
 
+class Id_typesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Id_types
+        fields = ['id', 'name']
+
+class Item_categoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item_categories
+        fields = ['id', 'name']
+
+class Item_statusesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item_statuses
+        fields = ['id', 'name']
+
+class Borrow_statusesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrow_statuses
+        fields = ['id', 'name']
+
 class UserSerializer(serializers.ModelSerializer):
     u_faculty = serializers.PrimaryKeyRelatedField(queryset=Faculty.objects.all())
     u_department = serializers.PrimaryKeyRelatedField(queryset=Department.objects.all())
@@ -24,3 +44,23 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'u_name', 'u_password', 'u_email', 'u_tel', 'u_faculty', 'u_department', 'u_privilege', 'u_create_at', 'u_update_at']
+
+class ItemSerializer(serializers.ModelSerializer):
+    item_id_type = serializers.PrimaryKeyRelatedField(queryset=Id_types.objects.all())
+    item_category = serializers.PrimaryKeyRelatedField(queryset=Item_categories.objects.all())
+    item_faculty = serializers.PrimaryKeyRelatedField(queryset=Faculty.objects.all())
+    item_department = serializers.PrimaryKeyRelatedField(queryset=Department.objects.all())
+    item_status = serializers.PrimaryKeyRelatedField(queryset=Item_statuses.objects.all())
+    item_borrow_status = serializers.PrimaryKeyRelatedField(queryset=Borrow_statuses.objects.all())
+
+    class Meta:
+        model = Item
+        fields = ['id', 'item_id', 'item_id_type', 'item_name', 'item_category', 'item_description', 'item_faculty', 'item_department', 'item_status', 'item_borrow_status', 'item_note', 'item_img_url', 'item_create_at', 'item_update_at']
+
+class Borrow_infoSerializer(serializers.ModelSerializer):
+    b_item = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
+    b_user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Borrow_info
+        fields = ['id', 'b_item', 'b_user', 'b_location', 'b_note', 'b_borrow_time', 'b_return_time', 'b_create_at', 'b_update_at']
